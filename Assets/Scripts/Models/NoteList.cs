77d8c4550045fcdfc7aeac660b441950ee268a3f
rldejesusm <ramon@dejesus.services>
2025-05-09 20:34:16 -0400

    finished comments for NoteList.cs Define and Document Note Class Structure #24

--- Assets/Scripts/Models/NoteList.cs
+++ Assets/Scripts/Models/NoteList.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 namespace ARStickyNotes.Models
 {
+    /// <summary>
+    /// Represents a serializable wrapper around a list of <see cref="Note"/> objects.
+    /// This is necessary because Unity's <c>JsonUtility</c> does not support direct serialization of lists or arrays.
+    /// </summary>
     [Serializable]
     public class NoteList
     {
-        /*
-            Using this class instead of directly using the list in the code 
-            because a direct list/array cannot be converted to json with JsonUtility.
-        */
+        /// <summary>
+        /// The list of notes contained in this wrapper.
+        /// </summary>
         [SerializeField] public List<Note> Items = new List<Note>();
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NoteList"/> class with an empty note list.
+        /// </summary>
         public NoteList() { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NoteList"/> class with a pre-existing list of notes.
+        /// </summary>
+        /// <param name="lst">A list of <see cref="Note"/> items to initialize the list with.</param>
         public NoteList(List<Note> lst)
         {
             Items = lst;
