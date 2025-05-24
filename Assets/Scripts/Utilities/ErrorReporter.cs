ca55db68b3763914b3a738b08725887afc0ecae0
rldejesusm <ramon@dejesus.services>
2025-05-24 13:11:00 -0400

    Add ToastManager integration to ErrorReporter and create meta files for UI components #51

--- Assets/Scripts/Utilities/ErrorReporter.cs
+++ Assets/Scripts/Utilities/ErrorReporter.cs
@@ -1,3 +1,4 @@
+using ARStickyNotes.UI;
 using UnityEngine;
 
 namespace ARStickyNotes.Utilities
@@ -20,3 +21,4 @@ namespace ARStickyNotes.Utilities
             ToastManager.Instance?.ShowToast(userMessage);
         }
     }
+}
