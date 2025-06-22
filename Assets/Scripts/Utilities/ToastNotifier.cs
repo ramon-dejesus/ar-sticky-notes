2b23ae707fcf38a5d720897a156cb3d8cb637c10
rldejesusm <ramon@dejesus.services>
2025-06-22 15:10:23 -0400

    #65 added color settings to toast notifier and error reporter classes

--- Assets/Scripts/Utilities/ToastNotifier.cs
+++ Assets/Scripts/Utilities/ToastNotifier.cs
@@ -8,12 +8,31 @@ namespace ARStickyNotes.Utilities
     public static class ToastNotifier
     {
         /// <summary>
-        /// Shows a toast message to the user.
+        /// Shows a default toast message to the user.
         /// </summary>
-        /// <param name="message">The message to display in the toast.</param>
+        /// <param name="message">The default message to display in the toast.</param>
         public static void Show(string message)
         {
-            ToastManager.Instance?.ShowToast(message);
+            ToastManager.Instance?.ShowToast(message, ToastType.Info);
         }
+
+        /// <summary>
+        /// Shows a success toast message to the user.
+        /// </summary>
+        /// <param name="message">The success message to display in the toast.</param>
+        public static void ShowSuccessMessage(string message)
+        {
+            ToastManager.Instance?.ShowToast(message, ToastType.Success);
+        }
+
+        /// <summary>
+        /// Shows a error toast message to the user.
+        /// </summary>
+        /// <param name="message">The error message to display in the toast.</param>
+        public static void ShowErrorMessage(string message)
+        {
+            ToastManager.Instance?.ShowToast(message, ToastType.Error);
+        }
+
     }
 }
