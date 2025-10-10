881211040291b9e32e25b478506ea0d8cf7aed19
darthrevan888 <darthrevan888@gmail.com>
2025-10-10 00:15:00 -0400

    Refactor ARStickyNotesController to improve event binding and error handling
    Signed-off-by: darthrevan888 <darthrevan888@gmail.com>

--- Assets/Scripts/UI/ARStickyNotesController.cs
+++ Assets/Scripts/UI/ARStickyNotesController.cs
@@ -1,61 +1,83 @@
-using System.Collections.Generic;
 using ARStickyNotes.Services;
 using ARStickyNotes.Utilities;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using UnityEngine.XR.ARFoundation;
-using UnityEngine.XR.Interaction.Toolkit.Inputs.Readers;
-using UnityEngine.XR.Interaction.Toolkit.Interactors;
-using UnityEngine.XR.Interaction.Toolkit.Samples.ARStarterAssets;
-using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 
 namespace ARStickyNotes.UI
 {
-
     public class ARStickyNotesController : MonoBehaviour
     {
+        /// <summary>
+        /// Reference to the Menu button in the UI.
+        /// </summary>
         [SerializeField]
         private Button MenuButton;
 
+        /// <summary>
+        /// Reference to the Menu options panel in the UI.
+        /// </summary>
         [SerializeField]
         private GameObject MenuOptionsPanel;
 
+        /// <summary>
+        /// Reference to the Whiteboard button in the UI.
+        /// </summary>
         [SerializeField]
         private Button WhiteboardButton;
 
+        /// <summary>
+        /// Reference to the Whiteboard prefab to be instantiated.
+        /// </summary>
         [SerializeField]
         private GameObject Whiteboard;
 
+        /// <summary>
+        /// Reference to the NoteManager for managing notes.
+        /// </summary>
         [SerializeField]
         private NoteManager NoteMan;
 
+        /// <summary>
+        /// Called when the script instance is being loaded.        /// 
+        /// </summary>
         void OnEnable()
         {
             try
             {
-                if (MenuButton != null)
-                {
-                    MenuButton.onClick.AddListener(ShowMenu);
-                }
-                else
-                {
-                    throw new System.Exception("MenuButton reference is missing in MenuManager.");
-                }
-                if (WhiteboardButton != null)
-                {
-                    WhiteboardButton.onClick.AddListener(ShowWhiteboard);
-                }
-                else
-                {
-                    throw new System.Exception("WhiteboardButton reference is missing in MenuManager.");
-                }
+                BindElements();
             }
             catch (System.Exception ex)
             {
                 ErrorReporter.Report("An error occurred while binding menu elements.", ex);
             }
         }
+
+        /// <summary>
+        /// Binds UI elements to their respective event handlers.
+        /// </summary>
+        void BindElements()
+        {
+            if (MenuButton != null)
+            {
+                MenuButton.onClick.AddListener(ShowMenu);
+            }
+            else
+            {
+                throw new System.Exception("MenuButton reference is missing in MenuManager.");
+            }
+            if (WhiteboardButton != null)
+            {
+                WhiteboardButton.onClick.AddListener(ShowWhiteboard);
+            }
+            else
+            {
+                throw new System.Exception("WhiteboardButton reference is missing in MenuManager.");
+            }
+        }
+
+        /// <summary>
+        /// Toggles the visibility of the menu options panel.
+        /// </summary>
         void ShowMenu()
         {
             try
@@ -71,23 +93,26 @@ namespace ARStickyNotes.UI
                 ErrorReporter.Report("An error occurred while binding menu elements.", ex);
             }
         }
+
+        /// <summary>
+        /// Displays the whiteboard and loads notes onto it.
+        /// </summary>
         void ShowWhiteboard()
         {
             try
             {
+                MenuOptionsPanel.SetActive(false);
                 if (Whiteboard == null)
                 {
                     throw new System.Exception("Whiteboard reference is missing.");
                 }
-                MenuOptionsPanel.SetActive(false);
-                var tmp = Instantiate(Whiteboard, transform);
-                var item = new ARSpawner().SpawnGameObject(tmp);
+                var item = Instantiate(Whiteboard, transform);
+                item = new ARSpawner().SpawnGameObject(item);
                 //item.GetComponent<NoteboardController>().LoadNotes(NoteMan.GetNotes());
             }
             catch (System.Exception ex)
             {
-                Debug.LogError(ex.ToString());
-                //ErrorReporter.Report("An error occurred while spawning the whiteboard.", ex);
+                ErrorReporter.Report("An error occurred while spawning the whiteboard.", ex);
             }
         }
     }
